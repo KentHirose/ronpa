d9cc34486c2832271e07c5b4f572848b54822e5b
misya11p <miya33ps@gmail.com>
2022-06-13 03:45:46 +0900

    susunda

--- app.py
+++ app.py
@@ -7,12 +7,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/visualization', methods=['POST'])
+@app.route('/results', methods=['POST'])
 def results():
     word = request.form.get('query')
+
+
+    # 仮 -------------------------------
+    # positions = [[0.2, 0.2], [0.7, 0.7]]
     titles = ['自然言語処理', 'コロナウイルス']
     links = ['http://abehiroshi.la.coocan.jp/']*2
-    return render_template('visualization.html', word=word, data=zip(titles, links))
+    positions = [[50, 50], [300, 300]]
+    colors = [[255, 0, 0], [0, 255, 255]]
+
+    return render_template(
+        'results.html',
+        word=word,
+        data=zip(titles, links),
+        positions=positions,
+        colors=colors
+        )
 
 if __name__ == '__main__':
     app.run(debug=True)
