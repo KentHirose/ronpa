f294b0620c45de861f9c2d5699250ae9b5f74e5f
misya11p <miya33ps@gmail.com>
2022-06-19 02:52:16 +0900

    kensakugamen

--- main_flow.py
+++ main_flow.py
@@ -1,12 +1,13 @@
 import sys; sys.path.insert(0, './modules')
 from paper_search import search
 from sent2vec import sent2vec
-from tours import pca, clustering, normalize
+from tours import pca, clustering, normalize, intervel
 
 def flow(query, n_papers=20):
     titles, links, abstracts = search(query, n_papers)
     vectors = sent2vec(abstracts)
-    positions = pca(vectors)
-    positions = normalize(positions)
+    vectors_2d = pca(vectors)
+    positions = normalize(vectors_2d)
+    positions = intervel(positions)
     clusters = clustering(vectors)
     return titles, links, positions, clusters
--- modules/tours.py
+++ modules/tours.py
@@ -1,6 +1,9 @@
+import random 
+import re
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import MinMaxScaler
+from sklearn import cluster, preprocessing, mixture
 
 def clustering(X: np.ndarray) -> np.ndarray:
     """クラスタリング. k-means?"""
@@ -21,6 +24,13 @@ def clustering(X: np.ndarray) -> np.ndarray:
         centroids =  new_centroids
     return cluster
 
+def clustering(X: np.ndarray) -> np.ndarray:
+    """クラスタリング. VBGMM"""
+    vbgm = mixture.BayesianGaussianMixture(n_components=10)
+    vbgm = vbgm.fit(X)
+    cluster = vbgm.predict(X)
+    return cluster
+
 def pca(data: np.ndarray) -> np.ndarray:
     """次元削減"""
     pca=PCA(n_components=2)
@@ -28,4 +38,45 @@ def pca(data: np.ndarray) -> np.ndarray:
     return data
 
 def normalize(X: np.ndarray) -> np.ndarray:
+    """"0~100に正規化"""
     return MinMaxScaler((0, 100)).fit_transform(X)
+
+def intervel(data: np.ndarray) -> np.ndarray:
+    """間隔を調整する"""
+    for seq in range (100):
+        for i in range(data.shape[0]):
+            for j in range(data.shape[0]):
+                if i!=j:
+                    while data[i][1]-data[j][1]<3 and data[i][1]-data[j][1]>=0:
+                        data[i][1]+=0.1
+                    while data[i][1]-data[j][1]>-3 and data[i][1]-data[j][1]<0:
+                        data[i][1]-=0.1
+    return data
+
+def lang_judg(search_results_list: list) -> None:
+    """アブストラクトが英語かどうかを判定し、英語だった場合タイトルに置き換える"""
+    deliete = re.compile('[!"#$%&\'\\\\()*+,-./:;<=>?@[\\]^_`{|}~「」〔〕""〈〉『』【】＆＊・（）＄＃＠。、？！｀＋￥％]')
+    cleaned_text = list(deliete.sub('', search_results_list[2]))
+    num = int(len(cleaned_text)* 0.3)
+    nums = random.randint(0,(len(cleaned_text)-1))
+    count = 0
+    word_count = 0
+    while word_count <= num:
+        judge_word = cleaned_text[nums]
+        ans = re.match('^[a-zA-Z]+$', judge_word)
+        cleaned_text.pop(nums)
+        print(cleaned_text)
+        if ans != 'None':
+            count += 1
+            word_count += 1
+        elif ans == "None":
+            word_count += 1
+    if count > int(num/2):
+        search_results_list[2] = search_results_list[0]
+
+
+# l = [
+#     ["タイトル1", 0, '英あああああああああああああああああああ'],
+#     ["タイトル2", 0, 'aaaaaaaabbbbbbbbbbbbbb']]
+# lang_judg(l)
+# print(l)
