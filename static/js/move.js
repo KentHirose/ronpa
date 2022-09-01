633f8a376d7c75211ee47e641253c114d243fd5d
misya11p <miya33ps@gmail.com>
2022-09-02 06:13:49 +0900

    Little improvement in operability

---
+++ static/js/move.js
@@ -0,0 +1,22 @@
+var isMousedDown = false;
+var tmpX;
+var tmpY;
+var target = document.getElementById('elem');
+
+window.addEventListener('mousedown', e => {
+    isMousedDown = true;
+    tmpX = e.clientX;
+    tmpY = e.clientY;
+    target.style.left = '30px'
+});
+
+window.addEventListener('mouseup', () => {
+    isMousedDown = false;
+});
+
+
+var rect = target.getBoundingClientRect();
+var targetX;
+var targetY;
+var moveX;
+var moveY;
